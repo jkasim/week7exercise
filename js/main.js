d09febd4409627be93328aeb8feac600220f904c
Josh Kasim <kasimjosh22@gmail.com>
2018-03-24 16:09:17 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//console.log("connected");
+
+// log every third number between 0-100
+
+//for(var i = 0; i <= 100; i += 1) {
+	//if(i % 3 == 0) {
+ //console.log(i);
+//}
+//}
+
+//Build a website which prompts a user for their first name, their last name, and their age in years. Then, your page should show an alert which greets the user by their full concatenated name, and then writes the user’s age IN MONTHS and their age IN DAYS in a complete sentence to the document.
+
+
+//var firstName = prompt ("what is your first name?")
+//var lastName = prompt ("what is your last name?")
+//var age = prompt("how old are you?")
+
+//alert("Hello, " + firstName + " " + lastName);
+
+//document.write("you are " + age + "," + " " + age*12 + " in months," + " and" + " " + age*365 + " in days!");
+
+
