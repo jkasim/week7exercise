9d32c76102f37891080fc5497450519889b8e050
Josh Kasim <kasimjosh22@gmail.com>
2018-03-31 13:48:23 -0700

    new commit

--- js/main.js
+++ js/main.js
@@ -1,22 +1,87 @@
 //console.log("connected");
 
-// log every third number between 0-100
 
-//for(var i = 0; i <= 100; i += 1) {
-	//if(i % 3 == 0) {
- //console.log(i);
-//}
-//}
+var horoscopes = [
+	{
+		zodiacName: 'Capricorn',
+		zodiacImage: 'img/cap.jpg',
+		zodiacSign: 'Ambitous, active minds, need to be in control.'
+	},
+	{
+		zodiacName: 'Aquarius',
+		zodiacImage: 'img/aquarius.jpg',
+		zodiacSign: 'Opportunist, does not care what others think, work for new ideas.'
+	},
+	{
+		zodiacName: 'Pisces',
+		zodiacImage: 'img/pisces.jpg',
+		zodiacSign: 'Sensitive, reserved, esacpe from reality, very good listener and friend.'
+	},
+	{
+		zodiacName: 'Aries',
+		zodiacImage: 'img/aries.jpg',
+		zodiacSign: 'Independent, courageous, enjoy leading others.'
+	},
+	{
+		zodiacName: 'Taurus',
+		zodiacImage: 'img/taurus.jpg',
+		zodiacSign: 'Fight for what they want, easy going but can be stubborn.'
+	},
+	{
+		zodiacName: 'Gemini',
+		zodiacImage: 'img/gemini.jpg',
+		zodiacSign: 'Many sides to them, very talkative, always will go where they want to.'
+	},
+	{
+		zodiacName: 'Cancer',
+		zodiacImage: 'img/cancer.jpg',
+		zodiacSign: 'Marked by inconsistency, enjoy security unpredictable.'
+	},
+	{
+		zodiacName: 'Leo',
+		zodiacImage: 'img/leo.jpg',
+		zodiacSign: 'High self esteem, very devoted, kind and generous.'
+	},
+	{
+		zodiacName: 'Virgo',
+		zodiacImage: 'img/virgo.jpg',
+		zodiacSign: 'Constantly analyzing and thinking, enjoy betterment of oneself.'
+	},
+	{
+		zodiacName: 'Libra',
+		zodiacImage: 'img/libra.jpg',
+		zodiacSign: 'Diplomatic nature, get along with everyone, ambitous.'
+	},
+	{
+		zodiacName: 'Scorpio',
+		zodiacImage: 'img/scorpio.jpg',
+		zodiacSign: 'Very intense, questions everything, treats others with kindness and loyalty.'
+	},
+	{
+		zodiacName: 'Sagittarius',
+		zodiacImage: 'img/sagittarius.jpg',
+		zodiacSign: 'Positive outlook on life, vibrant personalities, enjoys meeting new people.'
+	}
 
-//Build a website which prompts a user for their first name, their last name, and their age in years. Then, your page should show an alert which greets the user by their full concatenated name, and then writes the user’s age IN MONTHS and their age IN DAYS in a complete sentence to the document.
+];
+
+var input = document.getElementById("userData");
+var myH1 = document.getElementById("name");
+var myP = document.getElementById("sign");
+var myImage = document.getElementById("icon");
 
 
-//var firstName = prompt ("what is your first name?")
-//var lastName = prompt ("what is your last name?")
-//var age = prompt("how old are you?")
+function getInfo() {
 
-//alert("Hello, " + firstName + " " + lastName);
+	for(var i = 0; i <= horoscopes.length; i++) {
+		
+		if(input.value == horoscopes[i].zodiacName) {
+			myH1.innerText = horoscopes[i].zodiacName;
+			myP.innerText = horoscopes[i].zodiacSign;
+			myImage.src = horoscopes[i].zodiacImage;
+			console.log()
+		}
 
-//document.write("you are " + age + "," + " " + age*12 + " in months," + " and" + " " + age*365 + " in days!");
+	};
 
-
+}
